13ed5c87c62e9f41e053e0d984c828f1b88b524c
Shirley Ramirez <shirley.mramirez@yahoo.com>
2023-10-17 08:54:00 -0700

    Update postsSagas.js

--- src/sagas/postsSagas.js
+++ src/sagas/postsSagas.js
@@ -15,7 +15,7 @@ export function* fetchPosts(action) {
 /*
   Alternatively you may use takeLatest.
 
-  Does not allow concurrent fetches of user. If "USER_FETCH_REQUESTED" gets
+  Does not allow concurrent fetches of user. If "GET_POSTS" gets
   dispatched while a fetch is already pending, that pending fetch is cancelled
   and only the latest one will be run.
 */
